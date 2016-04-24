144754aa7eac5fc4d9b66095e631bb007743f5a7
Julian De Angelis <juliandeangelis@Julians-MBP.lan>
2016-04-24 17:48:27 -0400

    scroll up works

--- TouchlessMouse/src/CustomListener.java
+++ TouchlessMouse/src/CustomListener.java
@@ -1,6 +1,9 @@
 import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.Type;
 import java.awt.Dimension;
 import java.awt.Robot;
+import java.awt.event.*;
+
 
 /**
  * Created by juliandeangelis on 2016-04-24.
@@ -27,7 +30,7 @@ public class CustomListener extends Listener {
         InteractionBox box = frame.interactionBox();
         for (Finger f : frame.fingers()) {
             if (f.type() == Finger.Type.TYPE_INDEX) {
-                Vector fingerPos = f.tipPosition();
+                Vector fingerPos = f.stabilizedTipPosition();
                 Vector boxFingerPos = box.normalizePoint(fingerPos);
 
                 Dimension screen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
@@ -35,7 +38,23 @@ public class CustomListener extends Listener {
             }
         }
 
-
+        for (Gesture g : frame.gestures()) {
+            if (g.type() == Type.TYPE_CIRCLE) {
+                CircleGesture circle = new CircleGesture();
+                if (circle.pointable().direction().angleTo(circle.normal()) <=  Math.PI / 4){
+                    robot.mouseWheel(1);
+                    try { Thread.sleep(50); } catch (Exception e) { }
+                    }
+                else {
+                    robot.mouseWheel(-1);
+                    try { Thread.sleep(50); } catch (Exception e) { }
+                }
+            }
+            else if (g.type() == Type.TYPE_SCREEN_TAP) {
+                robot.mousePress(InputEvent.BUTTON1_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            }
+        }
     }
 
 }
